c0a7eb65ace8f108df1c84fb0c7723325d21cfcc
vigneshk94 <vigneshk@infrrd.ai>
2019-08-12 09:48:51 +0530

    format change

--- provider.tf
+++ provider.tf
@@ -3,7 +3,7 @@
 ####################################################################
 
 provider "aws" {
-    region = "${var.region}"
-    access_key = "${var.access_key}"
-    secret_key = "${var.secret_key}"
+  region     = "${var.region}"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
 }
--- terraform.tf
+++ terraform.tf
@@ -1,19 +1,19 @@
 resource "aws_instance" "master" {
-    ami           = "${var.ami}"
-    instance_type = "${var.instance_type}"
-    key_name      = "${var.key_name}"
-    associate_public_ip_address = "true"
-    tags = {
-        name = "${var.name_of_the_instance}"
-    }
+  ami                         = "${var.ami}"
+  instance_type               = "${var.instance_type}"
+  key_name                    = "${var.key_name}"
+  associate_public_ip_address = "true"
+  tags = {
+    name = "${var.name_of_the_instance}"
+  }
 }
 
 resource "aws_instance" "node" {
-    ami           = "${var.ami}"
-    instance_type = "${var.instance_type}"
-    key_name      = "${var.key_name}"
-    associate_public_ip_address = "true"
-    tags = {
-        name = "${var.name_of_the_instance}"
-    }
+  ami                         = "${var.ami}"
+  instance_type               = "${var.instance_type}"
+  key_name                    = "${var.key_name}"
+  associate_public_ip_address = "true"
+  tags = {
+    name = "${var.name_of_the_instance}"
+  }
 }
